f6cf00654b4100f8b2465fda67cff18ddba73ea6
Shantanu Gupta <shantanugupta@Shantanus-MacBook-Air.local>
2026-07-05 18:57:36 +0530

    initial project setup

---
+++ src/main/java/com/shantanugupta/audittrail/AudittrailApplication.java
@@ -0,0 +1,13 @@
+package com.shantanugupta.audittrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudittrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudittrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shantanugupta/audittrail/AudittrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanugupta.audittrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudittrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
